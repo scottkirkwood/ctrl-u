2c7a7585ba26a429611d7e44342e0eabe33116e6
scottkirkwood <scottkirkwood@asta-laptop>
2009-09-21 12:01:49 -0300

    Workds for edit boxes, but not for the omnibox, yet.

--- ctrlu.js
+++ ctrlu.js
@@ -1,10 +1,10 @@
 window.addEventListener('keydown',
  function (evt) {
    // U = 85
-   // I = 73
-   // i = 105
    if (evt.ctrlKey && evt.keyCode == 85) {
-     window.alert('hi');
-     evt.preventDefault();
+     if (evt.target.type && evt.target.type == 'text') {
+       evt.target.value = '';
+       evt.preventDefault();
+     }
    }
 }, true);
